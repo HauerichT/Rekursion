cfd6acbf32c5390e18769c284f51cfef19e32647
timohaverich <timo.haverich@fh-bielefeld.de>
2022-12-19 21:56:55 +0100

    Finished

--- rekursion.py
+++ rekursion.py
@@ -1,15 +1,14 @@
 def hoch(i, x=2):
+    print(str(x) + "^" + str(i))
+
     if i == 0:
         return 1
     elif i == 1:
         return x
     elif i < 0:
         return 1 / hoch(i, x)
-    else:
+    elif i > 0:
         return x * hoch(i - 1, x)
 
 
-print(hoch(i=4, x=3))
-
-# 3*3^3
-# https://www.pst.ifi.lmu.de/Lehre/fruhere-semester/wise-11-12/infoeinf/zentraluebung/zentraluebung11.pdf
+print(hoch(i=3, x=4))
